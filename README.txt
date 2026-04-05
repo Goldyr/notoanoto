1df4ce135ba31fa7108b4a2e0910724671f10061
goldyr <enzoefb@hotmail.com>
2026-04-05 19:39:08 -0300

    removed default, opts need notes_path to work

--- README.txt
+++ README.txt
@@ -2,6 +2,11 @@ NOTOANOTO
 
 small lua plugin for creating/editing notes
 
+notes_path need the path to the notes
+  opts = {
+    notes_path = '~/Documents/notes/',
+  },
+
 links:
-TJ plugin from scratch
+TJ neovim plugin from scratch
 https://www.youtube.com/watch?v=VGid4aN25iI
--- lua/init.lua
+++ lua/init.lua
@@ -1,15 +1,16 @@
 -- community standard for module
 local M = {}
--- DEFAULT CONFIG
-M.config = {
-	notes_path = vim.fn.expand("~/Documents/notes"),
-}
+
+local command_name = "Note"
 
 M.setup = function(user_config)
-	-- nvim doesnt like lower cases so might as well make it clear this is mine
-	M.config = vim.tbl_extend("force", M.config, user_config or {})
-	vim.api.nvim_create_user_command("NOTE", function(opts)
-		if vim.fn.isdirectory(M.config.notes_path) == 0 then
+	if user_config.notes_path == nil then
+		print("notoanoto extension error: no notes_path")
+		return
+	end
+
+	vim.api.nvim_create_user_command(command_name, function(opts)
+		if vim.fn.isdirectory(user_config.notes_path) == 0 then
 			print("this directory does not exist")
 			return
 		end
@@ -19,14 +20,14 @@ M.setup = function(user_config)
 			input = opts.args .. ".md"
 		end
 
-		local full_path = M.config.notes_path .. input
+		local full_path = user_config.notes_path .. input
 		--vim.cmd.edit(full_path) also exists
 		vim.cmd("edit " .. full_path)
 	end, {
 		nargs = 1,
 		--NOTE: needed for autocomplete
 		complete = function(ArgLead, CmdLine, CursorPos)
-			local files = vim.fn.readdir(M.config.notes_path)
+			local files = vim.fn.readdir(user_config.notes_path)
 			--similar to js map filter
 			return vim.tbl_filter(function(file)
 				local match_position = file:find("^" .. ArgLead)
